ec771eeb63efad4b7e6b679530ab2560e5b605f4
imvvdxd-hue <imvvdxd@gmail.com>
2026-07-27 02:23:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,6 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# 2. Title & Cartoon Character Image
-st.title("Welcome to My Fun Page! 🚀")
-st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNnF9zhdoqkh_p8IzgeHW3LbDhKGVWAKmCIQ3YeRZ1_STu9EZnm6T2T6Hn&s=10")
-
-# 3. Audio / Song
-st.subheader("🎵 Listen to Kyary Pamyu Pamyu - CANDY CANDY")
-st.video("https://www.youtube.com/watch?v=UoK8DaJRDaM")
 
 # 4. Interactive Questions
 st.divider()
@@ -82,3 +75,13 @@ May Allah help you get past all the difficulties you are facing and bring lot of
 May Allah make you a successful cardiologist, may He help you in this world and akhirah
 May Allah keep your eyes, your smile, your laugh, your happiness the whole you safe from shaitan and any nazar
 Ameen.""")
+
+
+
+# 2. Title & Cartoon Character Image
+st.title("Welcome to My Fun Page! 🚀")
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNnF9zhdoqkh_p8IzgeHW3LbDhKGVWAKmCIQ3YeRZ1_STu9EZnm6T2T6Hn&s=10")
+
+# 3. Audio / Song
+st.subheader("🎵 Listen to Kyary Pamyu Pamyu - CANDY CANDY")
+st.video("https://www.youtube.com/watch?v=UoK8DaJRDaM")
