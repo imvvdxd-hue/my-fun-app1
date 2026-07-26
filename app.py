5f35f9aa24e2daab51f017b4ee1c743025a5dc38
imvvdxd-hue <imvvdxd@gmail.com>
2026-07-27 01:19:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.markdown(
 
 # 2. Title & Cartoon Character Image
 st.title("Welcome to My Fun Page! 🚀")
-st.image("cinnamoroll.png")
+st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNnF9zhdoqkh_p8IzgeHW3LbDhKGVWAKmCIQ3YeRZ1_STu9EZnm6T2T6Hn&s=10")
 
 # 3. Audio / Song
 st.subheader("🎵 Listen to Kyary Pamyu Pamyu - CANDY CANDY")
